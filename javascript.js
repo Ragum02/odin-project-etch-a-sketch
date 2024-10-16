4c495f65243384c5efac4c698462c2e2276b3542
Ragum02 <benjamin.souiket@gmail.com>
2024-10-16 09:27:12 +0200

    Grid creations with flexbox + JS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!")
+console.log("Hello, world!");
+
+const boxContainer = document.querySelector("#container");
+
+for(let i = 0; i < 256; i++){
+    let newId = 0;
+    const createBox = document.createElement("div");
+    createBox.className = "box";
+    createBox.innerText = i + 1;
+    boxContainer.appendChild(createBox);
+}
